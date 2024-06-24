f8f71256e4ff688b4cb65811f1e309de32b163fa
shravyareddy7 <shravya.erravally@zemosolabs.com>
2024-06-24 19:06:50 +0530

    Removed unused files and modified components

--- src/components/molecules/Location/index.tsx
+++ src/components/molecules/Location/index.tsx
@@ -1,21 +1,19 @@
-import { Stack } from '@mui/material'
-import TextComponent from '../../atoms/Text'
-import MapIcon from '../../../assets/map-pin.svg'
-import SvgIconComponent from '../../atoms/Icon'
-import Underline from '../../../assets/underline.svg'
-import React from 'react'
+import { Stack } from '@mui/material';
+import TextComponent from '../../atoms/Text';
+import SvgIconComponent from '../../atoms/Icon';
+import React from 'react';
+import { ICONS, TEXTS, ALT_TEXTS, ICON_SIZES, SPACING, MARGINS } from '../../../constants';
 
-const Location = () => {
+const Location: React.FC = () => {
   return (
-    <Stack direction='column' spacing={0.5} marginBottom={0}>
-        <Stack direction='row' spacing={2}>
-        <SvgIconComponent src={MapIcon} alt="Leaf Icon" width='18px' height='18px'/>  
-        <TextComponent variant='body1'>East Maredpally, Secunderabad</TextComponent>
-        </Stack>
-      
-        <SvgIconComponent src={Underline} ></SvgIconComponent>
+    <Stack direction='column' spacing={SPACING.SMALL} marginBottom={MARGINS.BOTTOM}>
+      <Stack direction='row' spacing={SPACING.MEDIUM}>
+        <SvgIconComponent src={ICONS.MAP_PIN} alt={ALT_TEXTS.MAP_PIN_ICON} width={ICON_SIZES.SMALL} height={ICON_SIZES.SMALL} />
+        <TextComponent variant='body1'>{TEXTS.LOCATION}</TextComponent>
+      </Stack>
+      <SvgIconComponent src={ICONS.UNDERLINE} />
     </Stack>
-  )
-}
+  );
+};
 
-export default Location
+export default Location;
--- src/components/organism/Header.tsx
+++ src/components/organism/Header.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { AppBar, Box, Toolbar, Stack } from '@mui/material';
-import IconAndText from '../molecules/IconAndText';
+import LogoAndName from '../molecules/LogoAndName';
 import Location from '../molecules/Location';
-import ProfileComponent from '../molecules/ProfileAndUpdates';
+import SvgIconComponent from '../atoms/Icon';
+import CircularImageCard from '../atoms/Image';
+import {ICONS,ALT_TEXTS,ICON_SIZES} from "../../constants"
 
 const HeaderComponent = () => {
   return (
@@ -10,13 +12,17 @@ const HeaderComponent = () => {
       <Toolbar>
         <Stack direction="row" alignItems="center" width="100%">
           <Box flexGrow={1}>
-            <IconAndText />
+            <LogoAndName/>
           </Box>
           <Box>
             <Location />
           </Box>
           <Box flexGrow={3} display="flex" justifyContent="flex-end">
-            <ProfileComponent />
+          <Box display='flex' flexDirection='row' alignItems='center' gap={2}>
+              <SvgIconComponent src={ICONS.MESSAGES} alt={ALT_TEXTS.MESSAGES} width={ICON_SIZES.MEDIUM} height={ICON_SIZES.MEDIUM}/>
+              <SvgIconComponent src={ICONS.NOTIFICATIONS} alt={ALT_TEXTS.NOTIFICATIONS}  width={ICON_SIZES.MEDIUM} height={ICON_SIZES.MEDIUM}/>
+              <CircularImageCard imagePath={ICONS.PROFILE}/>
+          </Box>
           </Box>
         </Stack>
       </Toolbar>
